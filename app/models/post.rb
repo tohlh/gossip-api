147ae51ea07f2854b8c2c6104acf39f5a520b560
tohlh <tohliheng@gmail.com>
2022-12-21 22:28:33 +0800

    minimum title length

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :tag_assignments, dependent: :destroy
   has_many :tags, through: :tag_assignments
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 8 }
 end
