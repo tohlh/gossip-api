12be2ce1541b16881f0d6422347cb49764bc64a0
tohlh <tohliheng@gmail.com>
2022-12-21 17:57:35 +0800

    add Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_084548) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_095527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "tags", force: :cascade do |t|
     t.string "title"
@@ -26,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_21_084548) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
