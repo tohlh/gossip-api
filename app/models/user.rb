960a1fe484fcb6e28d2121df2a8e1141a8eba8be
tohlh <tohliheng@gmail.com>
2022-12-21 18:29:29 +0800

    user model validation and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts, dependent: :destroy
-  validates :username, presence: true, uniqueness: true
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: { minimum: 5 },
+            format: { without: /\s/, message: "only allows no space" }
   validates :password,
-             length: { minimum: 6 },
-             if: -> { new_record? || !password.nil? }
+            presence: true,
+            length: { minimum: 8 },
+            if: -> { new_record? || !password.nil? }
+  validates :password_confirmation,
+            presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid save user" do
+    user = User.new(name: "Valid Name",
+                    username: "valid_username",
+                    password: "password123",
+                    password_confirmation: "password123")
+    assert user.save
+  end
+
+  test "should not save when invalid username" do
+    user = User.new(name: "Valid Name",
+                    username: "invalid username",
+                    password: "password123",
+                    password_confirmation: "password123")
+    assert_not user.save
+  end
+
+  test "should not save when username is less than 5 characters" do
+    user = User.new(name: "Valid Name",
+                    username: "1234",
+                    password: "password123",
+                    password_confirmation: "password123")
+    assert_not user.save
+  end
+
+  test "should not save when no username" do
+    user = User.new(name: "Valid Name",
+                    password: "password123",
+                    password_confirmation: "password123")
+    assert_not user.save
+  end
+
+  test "should not save when unmatching password_confirmation" do
+    user = User.new(name: "Valid Name",
+                    username: "valid_username",
+                    password: "password123",
+                    password_confirmation: "password1234")
+    assert_not user.save
+  end
+
+  test "should not save when no password_confirmation" do
+    user = User.new(name: "Valid Name",
+                    username: "valid_username",
+                    password: "password123")
+    assert_not user.save
+  end
 end
