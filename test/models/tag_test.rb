dabfd7d151f355a87dc78744ef4bbcd4c5799d5f
tohlh <tohliheng@gmail.com>
2022-12-21 20:33:57 +0800

    Tag model tests

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid tag" do
+    tag = Tag.new(title: 'some_cool_tag_0')
+    assert tag.save
+  end
+
+  test "invalid caps tag" do
+    tag = Tag.new(title: 'SOME_COOL_TAG')
+    assert_not tag.save
+  end
+
+  test "invalid symbols tag" do
+    tag = Tag.new(title: 'some-cool-tag')
+    assert_not tag.save
+  end
+
+  test "tag too short" do
+    tag = Tag.new(title: 'a')
+    assert_not tag.save
+  end
+
+  test "tag too long" do
+    tag = Tag.new(title: 'abcdefghijklmnopqrstuvwxyzabc')
+    assert_not tag.save
+  end
 end
