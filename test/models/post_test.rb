1ac5a68ba883ffd0bc5fd55472629ce0cf869949
tohlh <tohliheng@gmail.com>
2022-12-23 12:58:57 +0800

    add post_test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Add Post" do
+    user = User.find_by(username: 'user1')
+    post = Post.new(title: 'Some cool title', content: 'Some cool content', user: user)
+    assert post.save
+  end
+
+  test "Invalid user post" do
+    user = nil
+    post = Post.new(title: 'Some cool title', content: 'Some cool content', user: user)
+    assert_not post.save
+  end
 end
