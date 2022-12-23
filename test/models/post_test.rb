5082436c6375c354d54bcc3e3a16152097dc0362
tohlh <tohliheng@gmail.com>
2022-12-23 14:00:02 +0800

    add post tests with invalid titles

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -12,4 +12,16 @@ class PostTest < ActiveSupport::TestCase
     post = Post.new(title: 'Some cool title', content: 'Some cool content', user: user)
     assert_not post.save
   end
+
+  test "Add post without title" do
+    user = User.find_by(username: 'user1')
+    post = Post.new(content: 'Some cool content', user: user)
+    assert_not post.save
+  end
+
+  test "Add post with blank title" do
+    user = User.find_by(username: 'user1')
+    post = Post.new(title: '', content: 'Some cool content', user: user)
+    assert_not post.save
+  end
 end
