899c553e058360bbb71ea2d14f0efc97a771ab36
tohlh <tohliheng@gmail.com>
2022-12-23 13:43:50 +0800

    add comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
   has_many :tag_assignments, dependent: :destroy
   has_many :tags, through: :tag_assignments
   validates :title, presence: true, length: { minimum: 8 }
