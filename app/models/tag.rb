18940d5ad219ae0eb03f01f6d25881c614594cfd
tohlh <tohliheng@gmail.com>
2023-01-04 22:08:29 +0800

    limit tag length to 20

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,6 @@ class Tag < ApplicationRecord
   has_many :posts, through: :tag_assignments
   validates :title,
             uniqueness: true,
-            format: { with: /\A(?=.{2,28}\z)([_a-z0-9]*)\z/,
-                      message: "only allows 2-28 characters of alphabets, numbers and underscores" }
+            format: { with: /\A(?=.{2,20}\z)([_a-z0-9]*)\z/,
+                      message: "only allows 2-20 characters of alphabets, numbers and underscores" }
 end
