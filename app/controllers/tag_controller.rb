0fd8f230b3f03b0fb2b07cfcf6dd38ef4679c91d
tohlh <tohliheng@gmail.com>
2023-01-04 20:36:49 +0800

    increase top tags count to 20

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -2,7 +2,7 @@ class TagController < ApplicationController
   before_action :authorize
 
   def get_tags
-    counts = TagAssignment.select(:tag_id).group(:tag_id).order('count(tag_id) DESC')[0, 10]
+    counts = TagAssignment.select(:tag_id).group(:tag_id).order('count(tag_id) DESC')[0, 20]
     tags = counts.map{|count| count.tag}
     render json: tags, each_serializer: TagSerializer
   end
